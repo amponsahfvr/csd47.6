7cd9fe60f4c601beded5b2a36722d47977984354
amponsahfvr <princeparker705@gmail.com>
2025-12-10 18:50:59 +0000

    Add files via upload

---
+++ basics of python.py
@@ -0,0 +1,23 @@
+playlist= ["FEIN", "Good days", "Mood",  "Peru", "Love Nwantiti"]
+print(playlist)
+
+
+top_artist = ["Beyonce", "Rema", "Wizkid", "Kizz Daniel", "Asake"]
+print(top_artist)
+
+my_music = [playlist, top_artist]
+print(my_music)
+print(len(my_music))
+
+print(playlist[2])
+playlist.append("Stay")
+print(playlist)
+
+playlist.insert(0, "Perfect")
+print(playlist)
+playlist.pop(1)
+playlist.pop(3)
+print(playlist)
+
+playlist.append("Blinding Lights")
+print(playlist)
